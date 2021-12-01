fa5c427353a4fca66f5c7845eef935fc84da30fb
MikeNotu <mikebateman98@gmail.com>
2021-12-01 01:00:21 -0600

    Navbar design and working Navbar completed, but I need to merge them

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
-
 import Page from './Page'
 import Testo from './components/Testo'
 import './App.css';
+import NavBar2 from './components/NavBar2';
 
 function App() {
   return (
     <Router>
-      <Navigation />
+      {/* <Navigation /> */}
+      <NavBar2/>
       <div className="container p-4">
         <Route path="/" exact component={Page} />
         <Route path="/testo" exact component={Testo} />
---
+++ src/components/NavBar2.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react"
+import { Link } from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  //   NavbarText,
+} from "reactstrap"
+
+const NavBar2 = (props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar light expand="md" className="navbar navbar-expand-lg navbar-dark bg-dark p-3 navbar-color">
+        <NavbarBrand href="/chess-champions/">
+          &nbsp;&nbsp;ChessChampions
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="https://bateman.studio/">My Website</NavLink>
+            </NavItem>
+            <NavItem>
+            <li className="nav-item">
+                                <Link to="/testo" className="nav-link">Create Test</Link>
+                            </li>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.linkedin.com/in/michael-bateman-979695194/">
+                My LinkedIn
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://www.chess.com/news/view/published-data-api">
+                API Used
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                GitHub
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem
+                  href="https://github.com/MikeNotu"
+                  className="dark-text"
+                >
+                  My GitHub
+                </DropdownItem>
+                <DropdownItem
+                  href="https://github.com/MikeNotu/chess-champions"
+                  className="dark-text"
+                >
+                  This Project's Repository
+                </DropdownItem>{" "}
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default NavBar2
